7a8887701d932e825a4fc21f2e0ebc084119c9e5
Stephanie Douglas <stephanietdouglas@gmail.com>
2021-06-17 11:15:40 -0400

    overplot clusters and tess sectors in stereogrphic projection

--- tess_sky_map.py
+++ tess_sky_map.py
@@ -46,8 +46,9 @@ def parse_s_region(s_region):
     return {'ra': ra, 'dec': dec}
 
 
-def stereographic_map(plot_sectors=[8,9,10], save=False, sector_colors=True, legend=True,
-                    hemisphere="South"):
+def stereographic_map(cluster=None, cluster_skycoord=None, plot_sectors=[8,9,10],
+                      save=True, sector_colors=True, legend=True,
+                      hemisphere="South"):
     """
     Plot TESS sectors (cycle 1 North or cycle 2 South) on a stereographic
     projection plot, in RA/Dec
@@ -65,19 +66,25 @@ def stereographic_map(plot_sectors=[8,9,10], save=False, sector_colors=True, leg
         regions[line['obs_id']] = (line['s_region'], line['sequence_number'])
 
     # Sector list for colors
+    # For now, I'm only considering Cycles 1 and 2, not the extended mission
     if sector_colors:
         sectors = np.arange(1,27)
         print(sectors)
         colors = cm.get_cmap('viridis', len(sectors))
         colors = colors(range(len(sectors)))
 
+    # Set up the axes
+    # Use cluster coordinates to set the center of the WCS
+    x_clu = cluster_skycoord.ra#geocentrictrueecliptic.lon.degree
+    y_clu = cluster_skycoord.dec#geocentrictrueecliptic.lat.degree
+
     w = WCS(naxis=2)
     w.wcs.crpix = [0,0] # center pix doesn't matter if no image
     w.wcs.cdelt = [0.01,0.01] # also doesn't matter if no image
 
-    # These clusters aren't near RA=0 so ignore wrapping issues
-    # approximate data center.
-    w.wcs.crval = [120,-35]
+    # These clusters aren't near RA=0 so ignore wrapping issues.
+    # Approximate data center.
+    w.wcs.crval = [np.nanmedian(x_clu.degree),np.nanmedian(y_clu.degree)]
     w.wcs.ctype = ["RA---STG", "DEC--STG"]
 
     ax = plt.subplot(projection=w)
@@ -86,20 +93,19 @@ def stereographic_map(plot_sectors=[8,9,10], save=False, sector_colors=True, leg
     ax.coords[0].set_axislabel('RA')
     ax.coords[1].set_axislabel('Dec')
 
-
-
     # print("axes set")
+    # Parse the edges of the camera footprints, and plot them on the sky
     for k, v in regions.items():
-        # if ("0014" in k) or ("0015" in k) or ("0016" in k) or ("0017" in k) or ("0018" in k):
-        # print(k,v)
 
-        if (int(k.split("-")[1][1:]) in plot_sectors)==False:
+        s = int(k.split("-")[1][1:])
+
+        if (s in plot_sectors)==False:
             continue
-        elif (int(k.split("-")[1][1:])>26):
+        elif (s>26):
             continue
-        elif (hemisphere=="South") and (int(k.split("-")[1][1:])>=14):
+        elif (hemisphere=="South") and (s>=14):
             continue
-        elif (hemisphere=="North") and (int(k.split("-")[1][1:])<14):
+        elif (hemisphere=="North") and (s<14):
             continue
         try:
             coords = parse_s_region(v[0])
@@ -124,17 +130,23 @@ def stereographic_map(plot_sectors=[8,9,10], save=False, sector_colors=True, leg
             ax.plot(ra,dec, color='b', alpha=0.5,
                     transform=ax.get_transform('world'))
 
+        # set_xlim(120,180,w,ax)
+        # set_ylim(-70,-40,w,ax)
 
-        ax.grid('on')
+    # Now plot the cluster points
 
-        set_xlim(120,180,w,ax)
-        set_ylim(-70,-40,w,ax)
+    ax.plot(x_clu,y_clu,'k.',ms=4,
+            transform=ax.get_transform('world'))
+    ax.grid('on')
 
+    set_xlim(np.nanmin(x_clu.degree)*0.97,np.nanmax(x_clu.degree)*1.03,w,ax)
+    set_ylim(np.nanmin(y_clu.degree)-3,np.nanmax(y_clu.degree)+3,w,ax)
 
-    # if save:
-    #     plt.savefig('plots/TESS_sectors_{0}_blank.png'.format(hemisphere.lower()))
+    ax.set_title(cluster.replace("_"," "))
 
-    plt.show()
+    if save:
+        plt.savefig('plots/TESS_sectors_stereographic_{0}.png'.format(cluster))
+
 
     return ax
 
@@ -178,4 +190,10 @@ def plot_clusters_sterographic():
         plt.show()
 
 if __name__=="__main__":
-    stereographic_map()
+    clusters = ["IC_2391","Collinder_135","NGC_2451A","NGC_2547"]
+    sector_list = [[8,9,10],[6,7,8],[7,8],[7,8,9]]
+    for i,cluster in enumerate(clusters):
+        cat = at.read(f"tables/{cluster}_crossmatch.csv",delimiter=",")
+        cat_pos = SkyCoord(cat["GAIAEDR3_RA"],cat["GAIAEDR3_DEC"],unit=u.degree)
+
+        stereographic_map(cluster,cat_pos,sector_list[i])
