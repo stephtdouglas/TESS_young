f4a0b201f679d7329a98e94f5a3c1446c2f2d9a1
Stephanie Douglas <stephanietdouglas@gmail.com>
2021-07-20 17:07:24 -0400

    marker props again

--- make_inspection_plots_tess.py
+++ make_inspection_plots_tess.py
@@ -508,12 +508,12 @@ def plot_sky(axes, TIC, wcs, pos, tess_img, gaia_pos):
 
     if (gaia_pos is not None) and (len(gaia_pos)>0):
         ax2.scatter(gaia_pos.ra,gaia_pos.dec,
-                    transform=ax2.get_transform('fk5'), s=8,
+                    transform=ax2.get_transform('fk5'), s=12,
                     linewidths=0.5,
                     edgecolor=cmap(0.1),facecolor=cmap(0.9),zorder=10)
-    ax2.scatter(pos.ra,pos.dec,transform=ax2.get_transform('fk5'), s=12,
+    ax2.scatter(pos.ra,pos.dec,transform=ax2.get_transform('fk5'), s=18,
                 linewidths=0.5,
-                edgecolor=cmap(0.7),facecolor=cmap(0.1),zorder=12,marker="s")
+                edgecolor=cmap(0.1),facecolor=cmap(0.75),zorder=12,marker="s")
     
 
     # ax2.plot([7,8.5],[9,9],color=cmap(0.5),transform=ax2.get_transform('pixel'))
