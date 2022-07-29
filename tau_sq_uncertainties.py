ce0ebdbf286f0f85cd01cc955823f04307a39180
Stephanie Douglas <stephanietdouglas@gmail.com>
2022-07-29 12:00:01 -0400

    file for calculating uncertainties with synthetic obs

---
+++ tau_sq_uncertainties.py
@@ -0,0 +1,44 @@
+import os, sys, glob, time
+
+import numpy as np
+import matplotlib.pyplot as plt
+import astropy.io.ascii as at
+
+from tau_sq import SpinModel, PeriodMassModel
+from tau_sq_run import run_all_models
+
+model_names = ["UpSco_Mattea2015","UpSco_Mattea2022","UpSco_ZeroTorque",
+               "WideHat8Myr_Mattea2015","WideHat8Myr_Mattea2022","WideHat8Myr_ZeroTorque"]
+
+def generate_synthetic_obs(model,age,period_scale,init_type,
+                           n_sets=100,n_per_set=500):
+
+    sm = SpinModel(model,age,period_scale,init_type=init_type)
+    if init_type!="kde":
+        sm.normalize()
+
+    for i in range(n_sets):
+        pmd = PeriodMassModel(sm,n_select=n_per_set,rng_seed=i)
+        pmd.select_obs(sm)
+
+        print(i)
+        run_all_models(pmd=pmd,output_filebase=f"tausq_syn_{i:04d}",
+                       models_to_plot=model_names,to_plot=False)
+
+        break
+
+if __name__=="__main__":
+
+    # Generate a fake model set
+    model="WideHat8Myr_Mattea2022"
+    age=80
+    period_scale="linear"
+    init_type="kde"
+    sm = SpinModel(model,age,period_scale,init_type=init_type)
+
+    sm.normalize()
+    pmd = PeriodMassModel(sm,n_select=500,rng_seed=9302)
+    pmd.select_obs(sm)
+
+    run_all_models(pmd=pmd,output_filebase="tausq_compare",
+                   models_to_plot=model_names)
