74c41b3d96e03cdf5a9e4b31fe5bda41715ee769
Stephanie Douglas <stephanietdouglas@gmail.com>
2023-05-03 10:20:05 -0400

    script to run all the tau-squared comparisons

---
+++ scripts/run_tausq.sh
@@ -0,0 +1,14 @@
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq1.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq2.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq3.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq4.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq5.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq6.yml
+
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq1_log.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq2_log.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq3_log.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq4_log.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq5_log.yml
+python tess_young/tau_sq/tau_sq_run.py -c config/tau_sq6_log.yml
+
